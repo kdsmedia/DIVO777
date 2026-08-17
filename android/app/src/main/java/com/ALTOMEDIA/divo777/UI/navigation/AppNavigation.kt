ac917d6379dab4f87124fd79f4d8a35b180ff765
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2026-08-18 04:52:21 +0700

    Implement AppNavigation with HomeScreen composable

---
+++ android/app/src/main/java/com/ALTOMEDIA/divo777/UI/navigation/AppNavigation.kt
@@ -0,0 +1,21 @@
+package com.altomedia.divo.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.altomedia.divo.ui.screens.HomeScreen
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "home"
+    ) {
+        composable("home") {
+            HomeScreen(navController = navController)
+        }
+    }
+}
